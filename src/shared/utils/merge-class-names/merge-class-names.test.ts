d94c3166ca926a07d644d4d9222d8fa39a1d1eb9
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2023-07-26 18:33:49 +0700

    test: add unit test for merge class names util

---
+++ src/shared/utils/merge-class-names/merge-class-names.test.ts
@@ -0,0 +1,25 @@
+import { mergeClassNames } from "./merge-class-names";
+
+describe("mergeClassNames", () => {
+  it("should return empty className when mergeClassNames is call without paramater", () => {
+    expect(mergeClassNames()).toBe("");
+  });
+
+  it('should return "text-red-500" when mergeClassNames is called with "text-red-500"', () => {
+    expect(mergeClassNames("text-red-500")).toBe("text-red-500");
+  });
+
+  it(`should return "text-red-500 bg-blue-200" when mergeClassNames is called with ${{
+    "text-red-500": true,
+    "font-bold": false,
+    "bg-blue-200": true,
+  }} `, () => {
+    const stubClassNames = {
+      "text-red-500": true,
+      "font-bold": false,
+      "bg-blue-200": true,
+    };
+
+    expect(mergeClassNames(stubClassNames)).toBe("text-red-500 bg-blue-200");
+  });
+});
