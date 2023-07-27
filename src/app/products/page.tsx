67d2ef38e94bf6f98d16ac07b03e49b8ded20f5c
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2023-07-27 17:15:49 +0700

    feat: add services folder

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -14,10 +14,10 @@ import { LoadingScreen } from "@/shared/components/loading-screen";
 import { Pagination } from "@/shared/components/pagination";
 import { SearchInput } from "@/shared/components/search-input";
 
-import { useProduct } from "@/hooks/products.hook";
-import { useCalculatePagination } from "@/hooks/pagination.hook";
-
 import { formatCurrency } from "@/shared/utils/format-currency";
+
+import { useProduct } from "@/shared/services/products.hook";
+import { useCalculatePagination } from "@/shared/services/pagination.hook";
 
 export default function Products() {
   const [page, setPage] = useState<number>(1);
@@ -71,34 +71,55 @@ export default function Products() {
 
   return (
     <div>
-      <div>
-        <SearchInput placeholder="Search Product" onSearch={handleSearch} />
+      <div className="flex">
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <SearchInput placeholder="Search Product" onSearch={handleSearch} />
+        </div>
       </div>
 
-      <Table className="w-full mt-3">
-        <TableHeader>
-          <TableRow>
-            <TableHead>No</TableHead>
-            <TableHead>Product Name</TableHead>
-            <TableHead>Brand</TableHead>
-            <TableHead>Price</TableHead>
-            <TableHead>Stock</TableHead>
-            <TableHead>Category</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {products.map((product) => (
-            <TableRow key={product.id} className="capitalize">
-              <TableCell>{product.id}</TableCell>
-              <TableCell className="font-medium">{product.title}</TableCell>
-              <TableCell>{product.brand}</TableCell>
-              <TableCell>{formatCurrency(product.price)}</TableCell>
-              <TableCell>{product.stock}</TableCell>
-              <TableCell>{product.category}</TableCell>
+      <div className="flex">
+        <Table className="w-full mt-3">
+          <TableHeader>
+            <TableRow>
+              <TableHead>No</TableHead>
+              <TableHead>Product Name</TableHead>
+              <TableHead>Brand</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>Stock</TableHead>
+              <TableHead>Category</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {products.map((product) => (
+              <TableRow key={product.id} className="capitalize">
+                <TableCell>{product.id}</TableCell>
+                <TableCell className="font-medium">{product.title}</TableCell>
+                <TableCell>{product.brand}</TableCell>
+                <TableCell>{formatCurrency(product.price)}</TableCell>
+                <TableCell>{product.stock}</TableCell>
+                <TableCell>{product.category}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
 
       <div className="mt-3">
         <Pagination
