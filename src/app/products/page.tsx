5422468c547428d500e96007dcbe08b84fcce05a
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2023-07-27 10:19:49 +0700

    feat: add pagination component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -13,6 +13,7 @@ import {
 import { Loading } from "@/shared/components/loading";
 
 import { useProduct } from "@/hooks/products.hook";
+import { Pagination } from "@/shared/components/pagination";
 
 export default function Products() {
   const { fetchProducts, isLoading, products } = useProduct();
@@ -26,29 +27,35 @@ export default function Products() {
   }
 
   return (
-    <Table className="w-full">
-      <TableHeader>
-        <TableRow>
-          <TableHead>No</TableHead>
-          <TableHead>Product Name</TableHead>
-          <TableHead>Brand</TableHead>
-          <TableHead>Price</TableHead>
-          <TableHead>Stock</TableHead>
-          <TableHead>Category</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {products.map((product) => (
-          <TableRow key={product.id} className="capitalize">
-            <TableCell>{product.id}</TableCell>
-            <TableCell className="font-medium">{product.title}</TableCell>
-            <TableCell>{product.brand}</TableCell>
-            <TableCell>{product.price}</TableCell>
-            <TableCell>{product.stock}</TableCell>
-            <TableCell>{product.category}</TableCell>
+    <div>
+      <Table className="w-full">
+        <TableHeader>
+          <TableRow>
+            <TableHead>No</TableHead>
+            <TableHead>Product Name</TableHead>
+            <TableHead>Brand</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Stock</TableHead>
+            <TableHead>Category</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {products.map((product) => (
+            <TableRow key={product.id} className="capitalize">
+              <TableCell>{product.id}</TableCell>
+              <TableCell className="font-medium">{product.title}</TableCell>
+              <TableCell>{product.brand}</TableCell>
+              <TableCell>{product.price}</TableCell>
+              <TableCell>{product.stock}</TableCell>
+              <TableCell>{product.category}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <div>
+        <Pagination />
+      </div>
+    </div>
   );
 }
