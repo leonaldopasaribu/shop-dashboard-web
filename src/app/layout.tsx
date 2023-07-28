4e1beeb0e14bd846a2b166a74b95a7d95b9c5ac3
Leonaldo Pasaribu <leonaldo.pasaribu@amarbank.co.id>
2023-07-28 11:04:55 +0700

    refactor: improve sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
-/* eslint-disable @next/next/no-sync-scripts */
 import { ReactNode } from "react";
 import type { Metadata } from "next";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
 
-import { SIDEBAR_MENU } from "@/shared/constants/menu.constant";
 import { Providers } from "@/providers";
+
+import { Sidebar } from "@/shared/components/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,56 +19,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <Providers>
       <html lang="en">
         <body className={inter.className}>
-          {" "}
           <main>
-            <button
-              data-drawer-target="separator-sidebar"
-              data-drawer-show="separator-sidebar"
-              aria-controls="separator-sidebar"
-              type="button"
-              className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            >
-              <span className="sr-only">Open sidebar</span>
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  clipRule="evenodd"
-                  fillRule="evenodd"
-                  d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-                ></path>
-              </svg>
-            </button>
-
-            <aside
-              id="separator-sidebar"
-              className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-              aria-label="Sidebar"
-            >
-              <div className="h-full px-3 py-4 overflow-y-auto bg-gray-200">
-                <ul className="space-y-2 font-medium">
-                  {SIDEBAR_MENU.map((data, id) => (
-                    <li key={id}>
-                      <a
-                        href="#"
-                        className="flex items-center p-2 text-black transition duration-75 rounded-lg hover:bg-gray-100 group"
-                      >
-                        <Image
-                          src={data.image}
-                          width={20}
-                          height={20}
-                          alt={data.alt}
-                        ></Image>
-                        <span className="ml-3">{data.label}</span>
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </aside>
+            <Sidebar />
 
             <div className="p-4 sm:ml-64">
               <div className="p-4 rounded-lg bg-white">
@@ -77,7 +28,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
               </div>
             </div>
           </main>
-          <script src="https://unpkg.com/flowbite@1.5.1/dist/flowbite.js"></script>
         </body>
       </html>
     </Providers>
--- src/shared/constants/menu.constant.ts
+++ src/shared/constants/menu.constant.ts
@@ -2,6 +2,7 @@ interface SidebarMenu {
   alt: string;
   href: string;
   image: string;
+  id: number;
   label: string;
 }
 
@@ -10,18 +11,21 @@ export const SIDEBAR_MENU: SidebarMenu[] = [
     alt: "dashboard-icon",
     href: "/",
     image: "/assets/icons/ic_dashboard.svg",
+    id: 1,
     label: "Dashboard",
   },
   {
     alt: "products-icon",
     href: "/products",
     image: "/assets/icons/ic_product.svg",
+    id: 2,
     label: "Products",
   },
   {
     alt: "carts-icon",
     href: "/carts",
     image: "/assets/icons/ic_cart.svg",
+    id: 3,
     label: "Carts",
   },
 ];
